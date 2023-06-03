297ac759056c0c2dce79f312caf15c99df8767cb
Muhammad Hijir Sajad <sajadhijir@gmail.com>
2023-06-03 13:32:37 +0800

    membuat kondisional pada navbar

--- src/components/layout/Navbar.vue
+++ src/components/layout/Navbar.vue
@@ -1,5 +1,37 @@
 <template>
-  <div class="navbar bg-transparent fixed z-50 py-5 lg:px-28 px-10 font-mulish">
+  <div v-if="isNavbar" class="navbar bg-[#00AF91] fixed z-50 py-5 lg:px-28 px-10 font-mulish">
+    <div class="navbar-start">
+      <div class="dropdown">
+        <button class="navbar-toggle md:hidden mx-3" :class="{ expanded: isExpanded }" @click="toggleExpand">
+          <div class="bg-[#00AF91]">
+            <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
+          </div>
+        </button>
+        <ul tabindex="0" class="menu menu-sm dropdown-content p-2 shadow bg-white border-[#00AF91] border-2 rounded-box w-52 mt-8">
+          <li><a>Beranda</a></li>
+          <li><a>Ayo Donasi</a></li>
+          <li><a>Buka Donasi</a></li>
+          <li><a>Tentang Kami</a></li>
+        </ul>
+      </div>
+      <a class="btn btn-ghost normal-case text-xl">
+        <img src="../../assets/images/logo.png" alt="" class="max-w-[50px] hidden md:flex" />
+      </a>
+    </div>
+    <div class="navbar-center hidden lg:flex">
+      <ul class="menu menu-horizontal px-1 text-white text-[20px] font-thin">
+        <li><a>Beranda</a></li>
+        <li><a>Ayo Donasi</a></li>
+        <li><a>Buka Donasi</a></li>
+        <li><a>Tentang Kami</a></li>
+      </ul>
+    </div>
+    <div class="navbar-end gap-3 font-bold">
+      <a class="py-2 px-8 bg-[#00AF91] border-white border-2 rounded-lg text-white cursor-pointer hidden md:flex">Masuk</a>
+      <a class="py-2 px-8 bg-white rounded-lg text-[#00AF91] cursor-pointer">Daftar</a>
+    </div>
+  </div>
+  <div v-else-if="!isNavbar" class="navbar bg-transparent fixed z-50 py-5 lg:px-28 px-10 font-mulish" v-on:scroll="changeBg">
     <div class="navbar-start">
       <div class="dropdown">
         <button class="navbar-toggle md:hidden mx-3" :class="{ expanded: isExpanded }" @click="toggleExpand">
@@ -38,10 +70,19 @@ import { ref, onMounted, onUnmounted } from "vue";
 export default {
   setup() {
     const isExpanded = ref(false);
+    const isNavbar = ref(false);
 
     const toggleExpand = () => {
       if (!isExpanded.value) {
         isExpanded.value = true;
+      }
+    };
+
+    const changeBg = () => {
+      if (window.scrollY <= 80) {
+        isNavbar.value = false;
+      } else if (window.scrollY > 80) {
+        isNavbar.value = true;
       }
     };
 
@@ -53,6 +94,7 @@ export default {
 
     onMounted(() => {
       document.addEventListener("click", handleClickOutside);
+      document.addEventListener("scroll", changeBg);
     });
 
     onUnmounted(() => {
@@ -62,6 +104,7 @@ export default {
     return {
       isExpanded,
       toggleExpand,
+      isNavbar,
     };
   },
 };
